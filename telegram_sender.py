3cf500152e2d678e3b18757a49ab0d1d0887d771
kmmorozov <kmmorozov@gmail.com>
2022-07-23 14:14:36 +0300

    Добавил отказоустойчивость

--- telegram_sender.py
+++ telegram_sender.py
@@ -24,19 +24,22 @@ def get_file_paths_from_directory(directory):
 
 
 if __name__ == '__main__':
+    retry_time = 600
     load_dotenv()
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
     token = os.getenv("TELEGRAM_TOKEN")
     time_out = get_args().freq
     directory = get_args().dir
     file_paths = get_file_paths_from_directory(directory)
-    try:
-        while True:
-            for file_path in file_paths:
+    while True:
+        for file_path in file_paths:
+            try:
                 send_photo_to_channel(token, chat_id, file_path)
                 sleep(int(time_out))
-            random.shuffle(file_path)
-    except telegram.error.NetworkError as e:
-        print('Ошибка подключения: {}'.format(e))
-    except telegram.error.InvalidToken as e:
-        print('Вы ввели неверный токен: {}.'.format(e))
+            except telegram.error.NetworkError as e:
+                print('Ошибка подключения: {}'.format(e))
+                sleep(retry_time)
+            except telegram.error.InvalidToken as e:
+                print('Вы ввели неверный токен: {}.'.format(e))
+                quit()
+        random.shuffle(file_path)
